92dd0b08e309d632c36bc1d712abe043a19a4957
sai kiran <kirannetha97@gmail.com>
2021-08-14 13:15:50 +0530

    todo dom
    tododom

---
+++ script.js
@@ -0,0 +1,5 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click')
